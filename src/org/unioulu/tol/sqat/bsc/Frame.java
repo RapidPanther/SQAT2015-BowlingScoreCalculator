007a828878ba2407ae2edced0586f0a9d2334cfe
somename <someemail>
2015-10-14 14:48:38 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -38,6 +38,22 @@ public class Frame {
 		//to be implemented
 		return 0;
 	}
+
+	public int getFirstThrow() {
+		return firstThrow;
+	}
+
+	public void setFirstThrow(int firstThrow) {
+		this.firstThrow = firstThrow;
+	}
+
+	public int getSecondThrow() {
+		return secondThrow;
+	}
+
+	public void setSecondThrow(int secondThrow) {
+		this.secondThrow = secondThrow;
+	}
 	
 	
 }
